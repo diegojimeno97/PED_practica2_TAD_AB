f695941254c6c6d8d975df97f4c6d3ba947856c5
DVdiego <diego.borja.mail@gmail.com>
2017-05-12 16:50:48 +0200

    Implementada Interfaz
    Implementada Interfaz

--- TAD_AB/src/practica2/ArbolBinario.java
+++ TAD_AB/src/practica2/ArbolBinario.java
@@ -1,15 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practica2;
 
-/**
- *
- * @author Diego
- */
-public class ArbolBinario {
+import excepciones.ArbolVacioExcepcion;
+
+public interface ArbolBinario<E> {
     
-    
+    public boolean esVacio();
+    public E raiz() throws ArbolVacioExcepcion;
+    public ArbolBinario<E>hijoIzq() throws ArbolVacioExcepcion;
+    public ArbolBinario<E>hijoDer() throws ArbolVacioExcepcion;
+    public boolean esta(E elmento);
+    public void setRaiz(E elemRaiz) throws ArbolVacioExcepcion;
+    public void setHijoIzq(ArbolBinario<E> hi)
+            throws ArbolVacioExcepcion;
+    public void setHijoDer(ArbolBinario<E> hd)
+            throws ArbolVacioExcepcion;
+    public void suprimir();
+    public void EliminarNodosInferiores();
 }
