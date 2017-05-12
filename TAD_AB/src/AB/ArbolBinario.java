1b05de4e98e98cb53ab2e3c1811b969f3ccc0952
diegojimeno97 <diegojimeno1997@gmail.com>
2017-05-12 17:15:49 +0200

    estructura del programa creada

--- TAD_AB/src/AB/ArbolBinario.java
+++ TAD_AB/src/AB/ArbolBinario.java
@@ -1,4 +1,4 @@
-package practica2;
+package AB;
 
 import excepciones.ArbolVacioExcepcion;
 
---
+++ TAD_AB/src/AB/NodoBinario.java
@@ -0,0 +1,40 @@
+
+package AB;
+
+public class NodoBinario<E> {
+    
+    private E elemento;
+    private NodoBinario<E> izq;
+    private NodoBinario<E> der;
+    
+    public NodoBinario(E e, NodoBinario<E> hi, NodoBinario<E> hd){
+        elemento = e;
+        izq = hi;
+        der = hd;
+    }
+    
+    public E getElemento(){
+        return elemento;
+    }
+    
+    public NodoBinario<E> getIzq(){
+        return izq;
+    }
+    
+    public NodoBinario<E> getDer(){
+        return der;
+    }
+    
+    public void setElemento(E e){
+        elemento = e;
+    }
+    
+    public void setIzq(NodoBinario<E> hi){
+        izq = hi;
+    }
+    
+    public void setDer(NodoBinario<E> hd){
+        der = hd;
+    }
+    
+}
